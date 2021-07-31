9259fdf42403a801d6992fb6f4a881b73b079dc3
Enoch Finley <enochfinley@gmail.com>
2021-07-31 17:42:13 -0500

    actually start using vue and ts

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <MainPage />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/MainPage.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-page">
+    <h1>Welcome to Enoch's Workshop</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class MainPage extends Vue {
+  msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
