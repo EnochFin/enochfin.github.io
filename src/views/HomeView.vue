f39d499e497b1c1e861a5fb043c88b8685734ec6
Enoch Finley <enochfinley@gmail.com>
2022-12-04 17:52:09 -0600

    consolidate items and fix routing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import WelcomeItem from '../components/WelcomeItem.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="welcome">
+    <WelcomeItem>
+      <template #icon>
+        <font-awesome-icon icon="fa-solid fa-cube" />
+      </template>
+      <template #heading><a href="https://github.com/EnochFin/enochfin.github.io">enochfin.github.io</a></template>
+      This is a github pages project that is designed to showcase my web-based projects and art.
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <font-awesome-icon icon="fa-solid fa-train" />
+      </template>
+      <template #heading>
+        <RouterLink to="/codingtrain">Coding Train</RouterLink>
+      </template>
+      This is a collection of Coding Train challenges that I've worked through.
+    </WelcomeItem>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .welcome {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+.art-item img {
+  width: 425px;
+  align-items: center;
+}
+
+.art-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
