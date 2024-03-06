fbcf758706165cbac63542005b5fb418fbee442c
Enoch Finley <enochfinley@gmail.com>
2024-03-06 15:44:45 -0600

    move buttons out

--- src/components/maze/Cell.vue
+++ src/components/maze/Cell.vue
@@ -3,6 +3,7 @@
     <v-line v-if="right" :config="rightLineConfig"></v-line>
     <v-line v-if="bottom" :config="bottomLineConfig"></v-line>
     <v-line v-if="top" :config="topLineConfig"></v-line>
+    <v-line v-if="line" :config="drawLineConfig"></v-line>
     <v-circle :config="visitedConfig"></v-circle>
 </template>
 
@@ -10,7 +11,7 @@
 import { ref } from "vue";
 
 
-const props = defineProps(['id', 'x', 'y', 'visited', 'walls']);
+const props = defineProps(['id', 'x', 'y', 'visited', 'walls', 'lineX', 'lineY']);
 
 const visited = props.visited;
 const walls = props.walls;
@@ -21,6 +22,8 @@ const y1 = padding + (props.y * scale);
 const x2 = x1 + scale;
 const y2 = y1 + scale;
 
+const line = props.lineX != 0 && props.lineY != 0;
+
 // lrtb
 const left = (walls & 1) == 1;
 const right = (walls & 2) == 2;
@@ -28,27 +31,35 @@ const top = (walls & 4) == 4;
 const bottom = (walls & 8) == 8;
 
 const wallColor = 'black';
+const lineColor = 'orange';
 
 const leftLineConfig = {
-    points: [x1, y1, x2, y1],
-    stroke: wallColor,
-    strokeWidth: 2,
-};
-const rightLineConfig = {
-    points: [x1, y2, x2, y2],
-    stroke: wallColor,
-    strokeWidth: 2,
-};
-const bottomLineConfig = {
-    points: [x2, y1, x2, y2],
-    stroke: wallColor,
-    strokeWidth: 2,
-};
-const topLineConfig = {
     points: [x1, y1, x1, y2],
     stroke: wallColor,
     strokeWidth: 2,
 };
+const rightLineConfig = {
+    points: [x2, y1, x2, y2],
+    stroke: wallColor,
+    strokeWidth: 2,
+};
+const bottomLineConfig = {
+    points: [x1, y2, x2, y2],
+    stroke: wallColor,
+    strokeWidth: 2,
+};
+const topLineConfig = {
+    points: [x1, y1, x2, y1],
+    stroke: wallColor,
+    strokeWidth: 2,
+};
+
+const drawLineConfig = {
+    points: [(x1 + x2) / 2, (y1 + y2) / 2, props.lineX, props.lineY],
+    stroke: lineColor,
+    strokeWidth: 2,
+}
+
 const visitedConfig = {
     x: x1 + (x2 - x1) * .7,
     y: y1 + (y2 - y1) * .3,
@@ -59,4 +70,3 @@ const visitedConfig = {
 }
 
 </script>
-  
--- src/components/maze/Window.vue
+++ src/components/maze/Window.vue
@@ -2,13 +2,16 @@
     <div ref="win" class="window-comp" data-id="window-component">
         <v-stage :config="stageConfig" class="window-stage">
             <v-layer>
-                <Maze v-if="!needUpdate" :width="winWidth" :height="winHeight" :cells="cells"></Maze>
+                <Maze :width="winWidth" :height="winHeight" :cells="cells"></Maze>
                 <Cell v-for="cell in cells" :key="cell.id" v-bind="cell"></Cell>
                 <v-text :config="textConfig"></v-text>
             </v-layer>
         </v-stage>
         <ul>
-            <li><button @click="handleInit">Init</button></li>
+            <li><button @click="handleInit">Break Walls</button></li>
+            <li><button @click="breakEntranceAndExit">Break Entrance and Exit</button></li>
+            <li><button @click="resetCellsVisited">Reset Cells Visited</button></li>
+            <li><button @click="setupCells">Reset Cells</button></li>
         </ul>
     </div>
 </template>
@@ -25,20 +28,28 @@ const winWidth = props.width;
 const winHeight = props.height;
 const size = 10;
 const num = size ** 2;
-let needUpdate = ref(false);
 
-let cellsInit = Array(num);
 
 let currId = 0;
 
 const getId = () => currId++;
 
-for (let ind = 0; ind < num; ind++) {
-    let yInd = Math.floor(ind / size);
-    let xInd = ind % size;
-    cellsInit[ind] = { id: getId(), x: xInd, y: yInd, visited: false, walls: 15 };
+const cellLeft = (v: number) => v & 14;    // 0111
+const cellRight = (v: number) => v & 13;   // 1011
+const cellTop = (v: number) => v & 11;     // 1101
+const cellBottom = (v: number) => v & 7;   // 1110
+
+const cells = ref([]);
+
+const setupCells = () => {
+    let cellsInit = Array(num);
+    for (let ind = 0; ind < num; ind++) {
+        let yInd = Math.floor(ind / size);
+        let xInd = ind % size;
+        cellsInit[ind] = { id: ind, x: xInd, y: yInd, visited: false, walls: 15, lineX: 0, lineY: 0 };
+    }
+    cells.value = cellsInit.slice();
 }
-const cells = ref(cellsInit.slice());
 
 const stageConfig = {
     width: winWidth,
@@ -62,7 +73,7 @@ const sleep = async () => {
     await new Promise(r => setTimeout(r, 500));
 }
 
-const getIndex = (i: number, j: number) => i * size + j;
+const getIndex = (i: number, j: number) => i + j * size;
 const randomIndex = (arr: any[]) => Math.floor(Math.random() * arr.length);
 
 const setVisited = (i: number, visited: boolean) => {
@@ -71,71 +82,96 @@ const setVisited = (i: number, visited: boolean) => {
     modCells[i].id = modCells[i].id + num;
 }
 
-const getNeedVisit = (i: number, j: number, visited: boolean) => {
+const isLogging = true;
+
+const log = (m: any, ...e: any[]) => {
+    if (isLogging) {
+        console.log(m, e);
+    }
+}
+
+const getNeedVisit = (x: number, y: number, visited: boolean, checkWalls: boolean = false) => {
     let needVisit = Array();
     // check left
-    if (i > 0) {
-        const lIndex = getIndex(i - 1, j);
-        if (cells.value[lIndex].visited == visited) {
-            needVisit.push(cells.value[lIndex]);
+    if (x > 0) {
+        const lIndex = getIndex(x - 1, y);
+        const cell = cells.value[lIndex];
+        if (cell.visited == visited && (!checkWalls || cellLeft(cell.walls) == cell.walls)) {
+            needVisit.push(cell);
+            log('added left', cell);
         }
     }
     // check right
-    if (i < size - 1) {
-        const rIndex = getIndex(i + 1, j);
-        if (cells.value[rIndex].visited == visited) {
-            needVisit.push(cells.value[rIndex]);
+    if (x < size - 1) {
+        const rIndex = getIndex(x + 1, y);
+        const cell = cells.value[rIndex];
+        if (cell.visited == visited && (!checkWalls || cellRight(cell.walls) == cell.walls)) {
+            needVisit.push(cell);
+            console.log('added right');
+            console.log(cell)
         }
     }
     // check top
-    if (j > 0) {
-        const tIndex = getIndex(i, j - 1);
-        if (cells.value[tIndex].visited == visited) {
-            needVisit.push(cells.value[tIndex]);
+    if (y > 0) {
+        const tIndex = getIndex(x, y - 1);
+        const cell = cells.value[tIndex];
+        if (cell.visited == visited && (!checkWalls || cellTop(cell.walls) == cell.walls)) {
+            needVisit.push(cell);
+            console.log('added top');
+            console.log(cell)
         }
     }
     // check bottom
-    if (j < size - 1) {
-        const bIndex = getIndex(i, j + 1);
-        if (cells.value[bIndex].visited == visited) {
-            needVisit.push(cells.value[bIndex]);
+    if (y < size - 1) {
+        const bIndex = getIndex(x, y + 1);
+        const cell = cells.value[bIndex];
+        if (cell.visited == visited && (!checkWalls || cellBottom(cell.walls) == cell.walls)) {
+            needVisit.push(cell);
+            console.log('added bottom');
+            console.log(cell)
         }
     }
     return needVisit
 };
 
-const breakWall = (i: number, j: number, toI: number, toJ: number) => {
-    const curr = cells.value[getIndex(i, j)];
-    const to = cells.value[getIndex(toI, toJ)];
-    // lrtb
-    const clearLeft = (v: number) => v & 14;    // 0111
-    const clearRight = (v: number) => v & 13;   // 1011
-    const clearTop = (v: number) => v & 11;     // 1101
-    const clearBottom = (v: number) => v & 7;   // 1110
-    if (toI < i) {
+const breakWall = (x: number, y: number, toX: number, toY: number) => {
+    const curr = cells.value[getIndex(x, y)];
+    const to = cells.value[getIndex(toX, toY)];
+    if (toX < x) {
         // left
-        curr.walls = clearLeft(curr.walls);
-        to.walls = clearRight(to.walls);
-    } else if (toI > i) {
+        console.log('break left');
+        curr.walls = cellLeft(curr.walls);
+        to.walls = cellRight(to.walls);
+    } else if (toX > x) {
         // right
-        curr.walls = clearRight(curr.walls);
-        to.walls = clearLeft(to.walls);
-    } else if (toJ < j) {
+        console.log('break right');
+        curr.walls = cellRight(curr.walls);
+        to.walls = cellLeft(to.walls);
+    } else if (toY < y) {
         // top
-        curr.walls = clearTop(curr.walls);
-        to.walls = clearBottom(to.walls);
-    } else if (toJ > j) {
+        console.log('break top');
+        curr.walls = cellTop(curr.walls);
+        to.walls = cellBottom(to.walls);
+    } else if (toY > y) {
         // bottom
-        curr.walls = clearBottom(curr.walls);
-        to.walls = clearTop(to.walls);
+        console.log('break bottom');
+        curr.walls = cellBottom(curr.walls);
+        to.walls = cellTop(to.walls);
     }
-    cells.value[getIndex(i, j)] = curr;
-    cells.value[getIndex(toI, toJ)] = to;
+    console.log(x, y, toX, toY);
+    cells.value[getIndex(x, y)] = curr;
+    cells.value[getIndex(toX, toY)] = to;
 }
 
 const breakEntranceAndExit = () => {
-    cells.value[0].walls = 0;
-    cells.value[cells.value.length - 1].walls = 0;
+    let entrance = cells.value[0];
+    let exit = cells.value[cells.value.length - 1];
+    entrance.walls = 0;
+    entrance.id += 100;
+    exit.walls = 0;
+    exit.id += 100;
+    cells.value[0] = entrance;
+    cells.value[cells.value.length - 1] = exit;
 }
 
 const resetCellsVisited = () => {
@@ -145,17 +181,12 @@ const resetCellsVisited = () => {
 }
 
 const breakWalls = () => {
-    breakWallsR(0, 0)
-    breakEntranceAndExit();
-    resetCellsVisited();
-    needUpdate.value = true;
-    needUpdate.value = false;
+    breakWallsR(0, 0);
     const temp = cells.value.slice();
     cells.value = temp;
     const nextTextConfig = textConfig.value;
     nextTextConfig.text = 'done!';
     textConfig.value = nextTextConfig;
-    console.log(cells.value);
 };
 
 const breakWallsR = (i: number, j: number) => {
@@ -168,12 +199,30 @@ const breakWallsR = (i: number, j: number) => {
         const randInd = randomIndex(needVisit);
         const dir = needVisit[randInd];
         needVisit.splice(randInd, 1);
-        const toI = dir.y;
-        const toJ = dir.x;
+        const toI = dir.x;
+        const toJ = dir.y;
         breakWall(i, j, toI, toJ);
+        // alert(`(${i}, ${j}) > (${toI}, ${toJ})`)
         breakWallsR(toI, toJ);
     }
 };
+
+const solve = () => {
+    solveR(0, 0);
+}
+
+const solveR = (i: number, j: number) => {
+    // TODO: implement step
+    const curr = cells.value[getIndex(i, j)];
+    setVisited(getIndex(i, j), true);
+    if (i == size && j == size) {
+        return true;
+    }
+    let needVisit = getNeedVisit(i, j, false, true);
+    needVisit.forEach(cell => {
+        console.log(cell);
+    });
+}
 </script>
 
 <style scoped>
@@ -189,4 +238,3 @@ const breakWallsR = (i: number, j: number) => {
     height: 100%;
 }
 </style>
-  
